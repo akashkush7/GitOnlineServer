690a9f43051466d09a83576ae6d32ca75ba9d371
Akash Kushwaha <akashkushwaha060@gmail.com>
2025-05-18 12:50:02 +0530

    for vercel

--- api/server.js
+++ api/server.js
@@ -95,6 +95,4 @@ app.post("/execute", async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+module.exports = app;
